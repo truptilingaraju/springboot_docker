df57e1c6b353f58ee3bf78d4a1db5b86d9030910
truptilingaraju <trupti.l01@testyantra.in>
2024-07-31 17:45:06 +0530

    addedd one more controller method

--- src/main/java/com/ty/Springboot_Docker/controller/DemoController.java
+++ src/main/java/com/ty/Springboot_Docker/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.ty.Springboot_Docker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,10 @@ public class DemoController {
     public String home(){
         return "Welcome Home";
     }
+
+    @GetMapping("/message/{name}")
+    public String message(@PathVariable String name){
+        return "welcome to springboot project" + name;
+    }
+
 }
